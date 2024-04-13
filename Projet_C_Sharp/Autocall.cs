13e5b93345f77ce78c5f36ae70ef2feefbfdf43e
Ayman Othmane <ayman.othmane@dauphine.eu>
2024-04-13 17:24:41 +0200

    update

--- Projet_C_Sharp/Autocall.cs
+++ Projet_C_Sharp/Autocall.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
+using Accord.Math;
 using Accord.Statistics.Distributions.Fitting;
 using Accord.Statistics.Distributions.Univariate;
 using MathNet.Numerics.Distributions;
@@ -12,13 +14,15 @@ namespace Autocall{
     private double S; // Current stock price
     private double K; // Strike price
     private double r; // Risk-free interest rate
+    private double barrier; //PDI Barrier
     private double div; // Dividend yield
     private double T; // Time to maturity (in years)
     private double vol; // Volatility of the underlying asset
     private double coupon; //Coupon of product
+    private double dt; //Time step
     private bool frequency;
     
-    public Athena(double S, double K, double r, double div, double T, double vol, double coupon, bool frequency)
+    public Athena(double S, double K, double r, double div, double T, double vol, double coupon, double dt, bool frequency)
     {
         this.S = S;
         this.K = K;
@@ -28,11 +32,143 @@ namespace Autocall{
         this.vol = vol;
         this.coupon = coupon;
         this.frequency = frequency;
-
-        double[][] ST = Generate_Paths.StockPaths(S, r, vol, dt, T/dt, 0, dt);  //double S0, double r, double vol, double dt, int nb_Simulations, int nb_steps, double mean_dist, double stdev_dist)
-
-
-
-    } 
+        this.dt = dt;
     }
+
+        public double Pricing()
+        {
+
+            double[][] Paths = Generate_Paths.StockPaths(S, r, vol, dt, Convert.ToInt32(T/dt), 0, dt);  //double S0, double r, double vol, double dt, int nb_Simulations, int nb_steps, double mean_dist, double stdev_dist)
+            double[][] Observations = ToolBox.ReturnDateFromIndex();
+            double[] Payoff = new double[10000];
+            double Price = 0;
+            
+            Observations = Observations.Transpose();
+            double[] Observation = new double[10000];
+
+            for (int i = 0; i <= Convert.ToInt32(T/dt)-1; i++)
+            {
+                Observation= Observations[i];
+                for (int j = 0; j <= Observation.Length; j++)
+                {
+                    if (Observation[j]<K)
+                    {
+                        Payoff[j]=0;
+                    }
+                    else{
+                        Payoff[j]=1+coupon;
+                    };
+                    
+                }
+                Price = Price + Math.Exp(-r) * Payoff.Average();
+
+            }
+            int k = Convert.ToInt32(T/dt)-1;
+            Observation= Observations[Observation.Length-1];
+            for (int j = 0; j <= Observation.Length; j++)
+            {
+                if (Observation[j]<K && Observation[j]/K>barrier)
+                {
+                    Payoff[j]=1;
+                }
+                else if (Observation[j]>K)
+                {
+                    Payoff[j]=1+coupon;
+                }
+                else{
+                    Payoff[j] = Observation[j]/K;
+                };
+                
+            }
+            Price = Price + Math.Exp(-r) * Payoff.Average();
+            return Price;
+        }
+
+    }
+
+    public class Phoenix{
+        
+    private double S; // Current stock price
+    private double K; // Strike price
+    private double r; // Risk-free interest rate
+    private double PDI_barrier; //PDI Barrier
+    private double Cpn_barrier; //Coupon Barrier
+    private double div; // Dividend yield
+    private double T; // Time to maturity (in years)
+    private double vol; // Volatility of the underlying asset
+    private double coupon; //Coupon of product
+    private double dt; //Time step
+    private bool frequency;
+    
+    public Phoenix(double S, double K, double r, double div, double T, double vol, double coupon, double dt, bool frequency)
+    {
+        this.S = S;
+        this.K = K;
+        this.PDI_barrier = PDI_barrier;
+        this.Cpn_barrier = Cpn_barrier;
+        this.r = r;
+        this.div = div;
+        this.T = T;
+        this.vol = vol;
+        this.coupon = coupon;
+        this.frequency = frequency;
+        this.dt = dt;
+    }
+
+        public double Pricing()
+        {
+
+            double[][] Paths = Generate_Paths.StockPaths(S, r, vol, dt, Convert.ToInt32(T/dt), 0, dt);  //double S0, double r, double vol, double dt, int nb_Simulations, int nb_steps, double mean_dist, double stdev_dist)
+            double[][] Observations = ToolBox.ReturnDateFromIndex();
+            double[] Payoff = new double[10000];
+            double Price = 0;
+            
+            Observations = Observations.Transpose();
+            double[] Observation = new double[10000];
+
+            for (int i = 0; i <= Convert.ToInt32(T/dt)-1; i++)
+            {
+                Observation= Observations[i];
+                for (int j = 0; j <= Observation.Length; j++)
+                {
+                    if (Observation[j]<K)
+                    {
+                        Payoff[j]=0;
+                    }
+                    else{
+                        Payoff[j]=1+coupon;
+                    };
+                    
+                }
+                Price = Price + Math.Exp(-r) * Payoff.Average();
+
+            }
+            int k = Convert.ToInt32(T/dt)-1;
+            Observation= Observations[Observation.Length-1];
+            for (int j = 0; j <= Observation.Length; j++)
+            {
+                if (Observation[j]<K && Observation[j]/K>barrier)
+                {
+                    Payoff[j]=1;
+                }
+                else if (Observation[j]>K)
+                {
+                    Payoff[j]=1+coupon;
+                }
+                else{
+                    Payoff[j] = Observation[j]/K;
+                };
+                
+            }
+            Price = Price + Math.Exp(-r) * Payoff.Average();
+            return Price;
+        }
+
+    }
+
+
+
 }
+
+
+
